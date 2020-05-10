c4db9462d0e392f184a9fa7e9e60e66122d9321b
Dawid Maślankiewicz <maslankiewiczd@gmail.com>
2020-05-10 21:27:43 +0200

    Add Mustache plugin

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+
+var templateList = document.getElementById('template-product-list').innerHTML;
+var carouselOutput = document.querySelector('.main-carousel');
+
+//Mustache.parse(templateList);
+
+var elements = '';
+
+for ( var i=0; i < productsData.length; i++){
+    //console.log(productsData);
+    elements += Mustache.render(templateList, productsData[i]);
+}
+console.log(elements);
+
+carouselOutput.innerHTML= elements;
+
+
 var elem =document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {    
     //cellAlign: 'left',
@@ -9,7 +26,7 @@ var flkty = new Flickity( elem, {
 var restartButton = document.querySelector('.restart');
 restartButton.addEventListener('click', function(){
     var cell1 = document.getElementById('carousel-cell1');
-    console.log(cell1)
+    console.log(cell1);
     flkty.selectCell(cell1);
 })
 
@@ -20,3 +37,4 @@ flkty.on('scroll', function(progress){
     progress = Math.max(0, Math.min(1, progress));
     progressBar.style.width = progress *100 +'%';
 });
+
