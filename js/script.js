80942862d996bd9bb03d4d75f41f2df297bfe961
Dawid Maślankiewicz <maslankiewiczd@gmail.com>
2020-05-11 22:10:32 +0200

    Add random map with own API key

--- js/script.js
+++ js/script.js
@@ -38,3 +38,10 @@ flkty.on('scroll', function(progress){
     progressBar.style.width = progress *100 +'%';
 });
 
+window.initMap= function(){
+    var uluru ={lat: -25.344, lng: 131.036};
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: uluru}
+    );
+    var marker = new google.maps.Marker({position: uluru, map: map})
+};
